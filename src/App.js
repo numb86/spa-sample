a434956a674bbcc52228db40295a6805fd25f78d
numb86 <16703337+numb86@users.noreply.github.com>
2020-03-25 19:30:35 +0900

    introduce react router v5

--- src/App.js
+++ src/App.js
@@ -1,7 +1,34 @@
 import React from 'react';
 import {createGlobalStyle} from 'styled-components';
+import {
+  BrowserRouter,
+  Switch,
+  Route,
+  Link,
+} from 'react-router-dom';
 
-export const App = () => <div><GlobalStyle />Hello React!</div>;
+const Home = () => <>This is <b>home</b> page.</>;
+const About = () => <>This is <b>about</b> page.</>;
+
+export const App = () => (
+  <>
+    <GlobalStyle />
+    <BrowserRouter>
+      <h1>Hello SPA!</h1>
+      <nav>
+        <Link to="./">to home</Link> <Link to="./about">to about</Link>
+      </nav>
+      <Switch>
+        <Route exact path="./">
+          <Home />
+        </Route>
+        <Route path="./about">
+          <About />
+        </Route>
+      </Switch>
+    </BrowserRouter>
+  </>
+);
 
 const GlobalStyle = createGlobalStyle`
   body {
